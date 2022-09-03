47312cde86b9ccd23e693fb94b04e3d59c985fb0
Edgar Cuarezma <ecuarezma@gmail.com>
2022-09-02 23:14:14 -0500

    wrapped squares in new div with id of board, test click functionality

--- synthGame.js
+++ synthGame.js
@@ -1,16 +1,22 @@
 const main = document.querySelector('#main');
+const board = document.querySelector('#board');
 
 // let's create our divs for the playing board.
 for (let i = 0; i < 3; i++){
   const newRow = document.createElement('div');
   newRow.className = 'row';
   newRow.setAttribute('id', `${i}`);
-  main.appendChild(newRow);
+  board.appendChild(newRow);
   for (let j = 0; j < 3; j++){
     const newSquare = document.createElement('div');
     newSquare.className = 'square';
     newSquare.setAttribute("id", `${j}`);
     newRow.appendChild(newSquare);
+    newSquare.addEventListener('click', function(e){
+      console.log(`Clicked on square ${i}, ${j}`);
+      console.log(e);
+      this.style.backgroundColor = "blue";
+    })
   }
 }
 
