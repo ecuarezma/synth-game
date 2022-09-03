0a0cbbee64b152a79cd3dbb67efd96f91decdb49
Edgar Cuarezma <ecuarezma@gmail.com>
2022-09-02 22:43:11 -0500

    Add id's to squares and rows

--- synthGame.js
+++ synthGame.js
@@ -4,10 +4,13 @@ const main = document.querySelector('#main');
 for (let i = 0; i < 3; i++){
   const newRow = document.createElement('div');
   newRow.className = 'row';
+  newRow.setAttribute('id', `${i}`);
   main.appendChild(newRow);
   for (let j = 0; j < 3; j++){
     const newSquare = document.createElement('div');
     newSquare.className = 'square';
+    newSquare.setAttribute("id", `${j}`);
     newRow.appendChild(newSquare);
   }
 }
+
