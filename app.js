575d427a6b7a7d1a8689e7dec96a59c5d3c98bec
Edgar Cuarezma <ecuarezma@gmail.com>
2022-09-05 18:44:26 -0500

    refactor application by breaking up functions into different files

--- app.js
+++ app.js
@@ -1,43 +1,7 @@
-const main = document.querySelector("#main");
-const board = document.querySelector("#board");
-//create header with game title
-const title = document.createElement("h1");
-title.className = "title";
-title.innerText = "selector ep";
-const header = document.querySelector("header");
-header.appendChild(title);
-
 // let's create our divs for the playing board.
-for (let i = 0; i < 3; i++) {
-  const newRow = document.createElement("div");
-  newRow.className = "row";
-  newRow.setAttribute("class", `row ${i}`);
-  board.appendChild(newRow);
-  for (let j = 0; j < 3; j++) {
-    //try to connect some music here to each square
-    let sample = new Audio(`./samples/${i}${j}.wav`);
-    //finish making square grid
-    const newSquare = document.createElement("div");
-    newSquare.className = "square";
-    newSquare.setAttribute("class", `square ${i}${j}`);
-    newRow.appendChild(newSquare);
+createGrid();
 
-    //add event listeners for each square
-    newSquare.addEventListener("mousedown", function (e) {
-      let selected = e.path[0];
-      console.log(selected);
-      console.log(`Clicked on square ${i}${j}`);
-      sample.play();
-      sample.loop = sample.loop ? false : true;
-      sample.addEventListener("play", function () {
-        selected.classList.add("selected");
-      });
-      sample.addEventListener("ended", function () {
-        selected.setAttribute("class", `square ${i}${j}`);
-      });
-    });
-    newSquare.addEventListener("mouseup", function () {
-      sample.loop = false;
-    });
-  }
-}
+//add event listeners for each square
+document.addEventListener("mousedown", function (e) {
+  onSelect(e);
+});
